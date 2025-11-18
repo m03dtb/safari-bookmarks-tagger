0d9cd2a450cc8e350673bc727c1a1b3e2d871576
m03dtb <m03dtb@users.noreply.github.com>
2025-11-18 20:05:10 +0100

    V18: safari bookmarks plist now loaded into table

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import pathlib
 import sys
+import json
 import re
+import plistlib
+from pathlib import Path
+
+from dataclasses import dataclass 
 from PySide6.QtGui import QKeySequence, QShortcut
 from rapidfuzz import fuzz
 from PySide6.QtCore import QSize, Qt, QAbstractTableModel
@@ -9,6 +15,68 @@ from PySide6.QtWidgets import (
     QTableView, QTableWidget, QListWidget, QTableWidgetItem
 )
 
+# CONSTANT VARIABLES 
+TAGS_JSON = Path("tags.json")
+BOOKMARKS_PLIST = Path("~/Library/Safari/Bookmarks.plist").expanduser()
+
+
+@dataclass
+class SafariBookmarks:
+    name: str
+    url: str
+
+def load_safari_bookmarks(plist_path: str | Path) -> list[SafariBookmarks]:
+    plist_path = Path(plist_path)
+    with plist_path.open("rb") as f:
+        root = plistlib.load(f)
+
+    bookmarks: list[SafariBookmarks] = []
+
+    def walk(node: dict):
+        # filders/containers 
+        for child in node.get("Children",[]):
+            if child.get("WebBookmarkType") == "WebBookmarkTypeLeaf" and "URLString" in child:
+                title = (
+                    child.get("URIDictionary", {}).get("title") or child.get("Title") or ""
+                )
+                url=child["URLString"]
+                bookmarks.append(SafariBookmarks(name=title, url=url))
+
+            # RECURSIVELY call function walk()
+            if "Children" in child:
+                walk(child)
+
+    # function call 
+    walk(root)
+    return bookmarks
+
+def load_tags() -> dict[str, list[str]]:
+    if not TAGS_JSON.exists():
+        return {}
+    with TAGS_JSON.open("r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    bm_tags = {}
+    for url, tags in data.items():
+        bm_tags[url] = tags if isinstance(tags, list) else str(tags).split(",")
+
+    return bm_tags
+
+def save_tags(tag_map: dict[str, list[str]]) -> None: 
+    with TAGS_JSON.open("w", encoding="utf-8") as f:
+        json.dump(tag_map, f, ensure_ascii=False, indent=2)
+
+def build_table_dict() -> dict[str, str]:
+    bookmarks = load_safari_bookmarks(BOOKMARKS_PLIST)
+    tag_map = load_tags()
+
+    table_dict: dict[str, str] = {}
+    for bm in bookmarks:
+        tags = tag_map.get(bm.url, [])
+        tags_str = ",".join(tags)
+        table_dict[bm.name] = tags_str 
+    
+    return table_dict
 
 class Table():
     def __init__(self, mydict, line) -> None:
@@ -239,8 +307,14 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
+        screen = QApplication.primaryScreen().geometry()
+        _, height =  screen.width(), screen.height()
+        self.setGeometry(0, 0, 700,height)
+
+
         self.line = None 
-        self.mydict = self.return_input_list()
+        # self.mydict = self.return_input_list()
+        self.mydict = build_table_dict()
         self.setWindowTitle("MyApp")
         self.table = Table(self.mydict, self.line)
         self.button = QPushButton("Pressme")
@@ -271,23 +345,25 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
 
 
-    def return_input_list(self):
-        mylist= ["el_num1", "el_num2", "el_num3","el_albert","el_chrissy",
-                "el_number", "el_num2b", "el_numb2", "el_num4a", "el_num4b",
-                "el_nmb41", "el_num41"]
-
-        mytags =  ["num1", "num2", "num3,num41,num4","num1","num2",
-                   "number", "num2b", "numb2", "anum2", "num4b",
-                   "nmb41", "num41,num2"]
-
-        dict_list_tags = {}
-        for elem, tag in zip(mylist, mytags) :
-            dict_list_tags[elem] = tag
-
-        return dict_list_tags
+    # def return_input_list(self):
+    #     mylist= ["el_num1", "el_num2", "el_num3","el_albert","el_chrissy",
+    #             "el_number", "el_num2b", "el_numb2", "el_num4a", "el_num4b",
+    #             "el_nmb41", "el_num41"]
+    #
+    #     mytags =  ["num1", "num2", "num3,num41,num4","num1","num2",
+    #                "number", "num2b", "numb2", "anum2", "num4b",
+    #                "nmb41", "num41,num2"]
+    #
+    #     dict_list_tags = {}
+    #     for elem, tag in zip(mylist, mytags) :
+    #         dict_list_tags[elem] = tag
+    #
+    #     return dict_list_tags
 
 
 def main():
+    # plist_path = pathlib.Path("~/Library/Safari/Bookmarks.plist").expanduser()
+    # print(plist_path)
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
